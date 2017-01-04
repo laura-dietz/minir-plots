039e1096c445cf239abe011cbb81f9d5017399b5
Laura Dietz <dietz@cs.umass.edu>
2017-01-04 13:36:24 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+setup(
+    name='minir-plots',
+    version='1.1',
+    packages=[],
+    url='minir-plots.cs.unh.edu',
+    license='BSD 3-Clause',
+    author='laura-dietz',
+    author_email='Laura.Dietz@unh.edu',
+    description='plotting trec_eval results',
+    install_requires=['pandas'],
+)
